eae44b5e7facc553eaf6fb307980b1bed87e29d5
0xParkr <109950023+0xParkr@users.noreply.github.com>
2024-12-07 22:35:33 -0500

    Clean install dependencies

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './components/LandingPage';
 import Flipbook from './components/Flipbook';
-import { SpeedInsights } from '@vercel/speed-insights/react';
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
@@ -31,7 +30,6 @@ function App() {
           </Routes>
         </main>
       </Router>
-      <SpeedInsights />
     </div>
   );
 }
